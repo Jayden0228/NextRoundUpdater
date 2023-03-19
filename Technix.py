a68eb18696f14268c7243c91ec9f4fc980f4e741
Jayden Viegas <jaydenviegas57@gmail.com>
2023-03-19 16:43:08 +0530

    Thread added along with some minor changes

--- Technix.py
+++ Technix.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 import Gspread as gs
+from threading import *
+from PIL import Image, ImageTk
 
 option=["Competitive_Coding",
         "Code_In_Chaos",
@@ -34,6 +36,8 @@ eventinfo={"Competitive_Coding":[3,''],
         "Meme":[3,'']
         }
 
+# img=ImageTk.PhotoImage(Image.open('Technix.jpeg'))
+
 class TechApp(Tk):
 
     teamlist=[]
@@ -46,8 +50,11 @@ class TechApp(Tk):
 
 
     def mainpg(self):
+
+        # mainframe=Frame(self, image=img)
         mainframe=Frame(self)
         mainframe.pack()
+        
 
         #Top text
         self.label=Label(mainframe, text="Select the Event!!", pady='20')
@@ -61,10 +68,25 @@ class TechApp(Tk):
         self.drop.config(width=len(max(option, key=len))+10, height=2)
         self.drop.pack()
 
+        self.pg=ttk.Progressbar(mainframe, length=100, orient=HORIZONTAL, mode='indeterminate')
+
         #Button
-        self.button=Button(mainframe, text=" Open ", command=lambda: [mainframe.destroy(), self.open()]).pack(pady='20')
+        self.button=Button(mainframe, text=" Open ", command=lambda: [mainframe.destroy(),  self.btnthread()]).pack(pady='20')
 
 
+    # def progress(self):
+    #     self.pg.pack(pady='5')
+    #     self.pg.start(10)
+
+    def btnthread(self):
+        t=Thread(target=self.open)
+        t.start()
+
+    def listtostr(self, tl):
+        strg=""
+        for x in tl:
+            strg+=x[0]+", "
+        return strg[0:len(strg)-2]
 
     #Adds the Team Name to the list
     def add(self):
@@ -74,7 +96,7 @@ class TechApp(Tk):
         if teamname not in self.teamlist and teamname!='':
             self.teamlist.append(teamname)
             print("List: ",self.teamlist)
-            self.label[i]['text']='Team List: '+str(self.teamlist)
+            self.label[i]['text']='Team List: '+ self.listtostr(self.teamlist)
     
     #Removes the Team Name from the list
     def remove(self):
@@ -83,7 +105,7 @@ class TechApp(Tk):
         if self.tree[i].item(tn)['values'] in self.teamlist:
             self.teamlist.remove(self.tree[i].item(tn)['values'])
             print("List: ",self.teamlist)
-            self.label[i]['text']='Team List: '+str(self.teamlist)
+            self.label[i]['text']='Team List: '+ self.listtostr(self.teamlist)
 
     #Writes the Team Names That Moves to the Next Round
     def moveToNxtRnd(self, event):
@@ -101,6 +123,7 @@ class TechApp(Tk):
 
     def open(self):
         self.note=ttk.Notebook(self)
+        self.note.winfo_geometry
         self.note.pack()
 
         # Getting the total number of rounds in the event
@@ -114,7 +137,7 @@ class TechApp(Tk):
 
         # Generating frames and adding it to the NoteBook
         for i in range(noOfRounds+1):
-            frames.append(Frame(self.note))
+            frames.append(Frame(self.note, width=250, height=300))
             frames[i].pack(fill="both", expand=1)
 
             if i!=noOfRounds:
@@ -126,8 +149,8 @@ class TechApp(Tk):
 
 
             #Creating Treeview
-            self.tree.append(ttk.Treeview(frames[i], column=("c1"), show="headings", height=8))
-            self.tree[i].column('c1', anchor="center")
+            self.tree.append(ttk.Treeview(frames[i], column=("c1"), show="headings",  height=8))
+            self.tree[i].column('c1', width=250, anchor="center")
             self.tree[i].heading('c1', text="Team Name")
 
             #Reading the Team Name and Displaying it
@@ -138,12 +161,15 @@ class TechApp(Tk):
 
             #Adding button for the rounds expect for the winners tab
             if i!=noOfRounds:
-                addbtn=Button(frames[i], text=f"Add To {self.txtmg[i+1]} Tab", command=self.add).pack(pady=5)
-                rembtn=Button(frames[i], text="Remove", command=self.remove).pack(pady=5)
+                addbtn=Button(frames[i], text="Add To The List", command=self.add).pack(pady=5)
+                rembtn=Button(frames[i], text="Remove From The List", command=self.remove).pack(pady=5)
                 finbtn=Button(frames[i], text="Add To Next Round", command=lambda: self.moveToNxtRnd(event)).pack(pady=5)
 
-            self.label.append(Label(frames[i]))
-            self.label[i].pack(pady=5)
+                canvas=Canvas(frames[i], height=10)
+                canvas.create_line(0,10,1000,10,width=3)
+                canvas.pack()
+                self.label.append(Label(frames[i], text='Team List: '))
+                self.label[i].pack(pady=5)
             
 
 
