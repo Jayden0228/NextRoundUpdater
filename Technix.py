8c5d209a0474169d6a3e99c1105e416ef82a6557
Jayden Viegas <jaydenviegas57@gmail.com>
2023-03-20 00:08:11 +0530

    Exception and Comments Added

--- Technix.py
+++ Technix.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
+from googleapiclient.errors import HttpError
 import Gspread as gs
 from Logic import *
 
@@ -16,13 +17,14 @@ class TechApp(Tk):
         self.geometry("900x550")
         self.mainpg()
 
-
+    # Main Frame Page
     def mainpg(self):
          
         mainframe=Frame(self)
         mainframe.pack()
         
-        self.img=ImageTk.PhotoImage(Image.open("Technix.png").resize((420,106), Image.ANTIALIAS))
+        #Technix Image
+        self.img=ImageTk.PhotoImage(Image.open("Technix.png").resize((420,134), Image.ANTIALIAS))
         self.imglab=Label(mainframe, image=self.img)
         self.imglab.pack(pady='5')
 
@@ -76,67 +78,97 @@ class TechApp(Tk):
         print("List: ",self.teamlist)
         self.label[i]['text']='Team List: '
 
-        self.eventframe.destroy()
-        self.open()
+        #Deleting the Childrens of Old Tree View
+        for item in self.tree[i+1].get_children():
+            self.tree[i+1].delete(item)
+
+        #Reading the New Team Name and Displaying it (Updated TreeView)
+        value=gs.read(eventinfo[event][1], self.txtmg[i+1])
+        for tname in value:
+            self.tree[i+1].insert('','end', values=str(tname[0]))
+        self.tree[i+1].pack()
 
 
+    # Displays the Event Frame 
     def open(self):
         self.eventframe=Frame(self)
         self.eventframe.pack()
 
+        # Back Button
         self.back=Button(self, text='Back', command=lambda:[self.eventframe.destroy(), self.mainpg(), self.back.destroy()])
-        # self.back.pack(padx=200, pady=200, side=LEFT)
-        self.back.place(x=70,y=20)
+        self.back.place(x=70,y=30)
 
-        self.img=ImageTk.PhotoImage(Image.open("Technix.png").resize((300,85), Image.ANTIALIAS))
+        # Technix Image
+        self.img=ImageTk.PhotoImage(Image.open("Technix.png").resize((300,105), Image.ANTIALIAS))
         self.imglab=Label(self.eventframe, image=self.img)
         self.imglab.pack(pady='12')
 
-        self.note=ttk.Notebook(self.eventframe)
-        self.note.pack()
+        try:
+            self.note=ttk.Notebook(self.eventframe)
+            self.note.pack()
 
-        # Getting the total number of rounds in the event
-        event=self.clicked.get()
-        noOfRounds=eventinfo[event][0]
+            # Getting the total number of rounds in the event
+            event=self.clicked.get()
+            noOfRounds=eventinfo[event][0]
 
-        frames=[]
-        self.txtmg=tabinfo(noOfRounds)
-        self.tree=[]
-        self.label=[]
+            frames=[]
+            self.txtmg=tabinfo(noOfRounds)
+            self.tree=[]
+            self.label=[]
 
-        # Generating frames and adding it to the NoteBook
-        for i in range(noOfRounds+1):
+            # Generating frames and adding it to the NoteBook
+            for i in range(noOfRounds+1):
 
-            frames.append(Frame(self.note, width=250, height=300))
-            frames[i].pack(fill="both", expand=1)
-            self.note.add(frames[i], text=self.txtmg[i])
-
-
-            #Creating Treeview
-            self.tree.append(ttk.Treeview(frames[i], column=("c1"), show="headings",  height=8))
-            self.tree[i].column('c1', width=250, anchor="center")
-            self.tree[i].heading('c1', text="Team Name")
-
-            #Reading the Team Name and Displaying it
-            value=gs.read(eventinfo[event][1], self.txtmg[i])
-            for tname in value:
-                self.tree[i].insert('','end', values=str(tname[0]))
-            self.tree[i].pack()
-
-            #Adding button for the rounds expect for the winners tab
-            if i!=noOfRounds:
-                Button(frames[i], text="Add To The List", command=self.add).pack(pady=5)
-                Button(frames[i], text="Remove From The List", command=self.remove).pack(pady=5)
-                Button(frames[i], text="Add To Next Round", command=lambda: self.moveToNxtRnd(event)).pack(pady=5)
-                # Button(frames[i], text="Add To Next Round", command=lambda: [btnthreadarg(self.moveToNxtRnd, (event))] ).pack(pady=5)
+                #Frames created and Added kto the notebook
+                frames.append(Frame(self.note, width=250, height=300))
+                frames[i].pack(fill="both", expand=1)
+                self.note.add(frames[i], text=self.txtmg[i])
 
 
-                canvas=Canvas(frames[i], height=10)
-                canvas.create_line(0,10,1000,10,width=3)
-                canvas.pack()
-                self.label.append(Label(frames[i], text='Team List: '))
-                self.label[i].pack(pady=5)
-            
+                #Creating Treeview
+                self.tree.append(ttk.Treeview(frames[i], column=("c1"), show="headings",  height=8))
+                self.tree[i].column('c1', width=250, anchor="center")
+                self.tree[i].heading('c1', text="Team Name")
+
+                #Reading the Team Name and Displaying it
+                value=gs.read(eventinfo[event][1], self.txtmg[i])
+                for tname in value:
+                    self.tree[i].insert('','end', values=str(tname[0]))
+                self.tree[i].pack()
+
+                # Adding button for the rounds expect for the winners tab
+                if i!=noOfRounds:
+                    Button(frames[i], text="Add To The List", command=self.add).pack(pady=5)
+                    Button(frames[i], text="Remove From The List", command=self.remove).pack(pady=5)
+                    Button(frames[i], text="Add To Next Round", command=lambda: self.moveToNxtRnd(event)).pack(pady=5)
+                    # Button(frames[i], text="Add To Next Round", command=lambda: [btnthreadarg(self.moveToNxtRnd, (event))] ).pack(pady=5)
+
+                    # Horizontal Line
+                    canvas=Canvas(frames[i], height=10)
+                    canvas.create_line(0,10,1000,10,width=3)
+                    canvas.pack()
+
+                    # Displays list of Team Names selected
+                    self.label.append(Label(frames[i], text='Team List: '))
+                    self.label[i].pack(pady=5)
+        
+        except HttpError as err:
+            self.note.destroy()
+            self.label=Label(self.eventframe, text="Unable to find the google sheet. Please check your internet connection or the google sheet Id")
+            self.label.config(font=("Times", 13))
+            self.label.pack(pady=40)
+
+        except (UnboundLocalError, TypeError):
+            self.note.destroy()
+            self.label=Label(self.eventframe, text="Variable is accessed before assignment")
+            self.label.config(font=("Times", 13))
+            self.label.pack(pady=40)
+
+        except Exception as err:
+            self.label=Label(self.eventframe, text="Error Occured!!")
+            self.label.config(font=("Times", 13))
+            self.label.pack(pady=40)
+            print(f"Unexpected {err=}, {type(err)=}")
 
 
 if __name__=="__main__":
